0ef0f6b47b1b2d984ba64c55cac4fc26bbf5c0c3
WMGray <1466787434@qq.com>
2025-04-14 21:42:59 +0800

    ✨ feat：主页面UI

--- ui/main_ui.go
+++ ui/main_ui.go
@@ -3,6 +3,7 @@ package ui
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
@@ -18,8 +19,10 @@ type MainUI struct {
 	currentContent *fyne.Container
 }
 
+var ui *MainUI // 全局变量，存储主界面实例
+
 func NewMainUI(window fyne.Window) *MainUI {
-	ui := &MainUI{
+	ui = &MainUI{
 		window: window,
 	}
 	ui.window.Resize(fyne.NewSize(1024, 768))
@@ -29,33 +32,13 @@ func NewMainUI(window fyne.Window) *MainUI {
 	ui.currentContent = ui.dataset
 
 	// 创建左侧导航栏
-	sidebar := container.NewVBox(
-		widget.NewLabel("图片数据集管理工具"),
-		widget.NewButtonWithIcon("数据集", theme.StorageIcon(), func() {
-			ui.showContent(ui.dataset)
-		}),
-		widget.NewButtonWithIcon("上传", theme.UploadIcon(), func() {
-			ui.showContent(ui.upload)
-		}),
-		widget.NewButtonWithIcon("验证", theme.ConfirmIcon(), func() {
-			ui.showContent(ui.validation)
-		}),
-		widget.NewButtonWithIcon("存储", theme.StorageIcon(), func() {
-			ui.showContent(ui.storage)
-		}),
-		widget.NewButtonWithIcon("导出", theme.DownloadIcon(), func() {
-			ui.showContent(ui.export)
-		}),
-		widget.NewButtonWithIcon("设置", theme.SettingsIcon(), func() {
-			ui.showContent(ui.settings)
-		}),
-	)
+	navBar := ui.createNavBar()
 
 	// 创建主内容区域
-	mainContent := container.NewMax(ui.currentContent)
+	mainContent := container.NewStack(ui.currentContent)
 
 	// 创建水平分割布局，左侧导航栏占 20%
-	split := container.NewHSplit(sidebar, mainContent)
+	split := container.NewHSplit(navBar, mainContent)
 	split.Offset = 0.2
 
 	// 设置窗口内容
@@ -64,6 +47,38 @@ func NewMainUI(window fyne.Window) *MainUI {
 	return ui
 }
 
+// createNavBar 创建左侧导航栏
+func (m *MainUI) createNavBar() fyne.CanvasObject {
+	// 功能按钮
+	topFuncBtns := container.NewVBox(
+		widget.NewButtonWithIcon("数据集", theme.StorageIcon(), func() {
+			ui.showContent(ui.dataset)
+		}),
+		widget.NewButtonWithIcon("上传", theme.UploadIcon(), func() {
+			ui.showContent(ui.upload)
+		}),
+	)
+
+	//	// 下半功能区 -- 账户、设置
+	btmFuncBtns := container.NewVBox(
+		widget.NewButtonWithIcon("账户", theme.AccountIcon(), func() {
+			ui.showContent(ui.validation)
+		}),
+		widget.NewButtonWithIcon("设置", theme.SettingsIcon(), func() {
+			ui.showContent(ui.settings)
+		}),
+	)
+
+	// 导航栏容器
+	return container.NewVBox(
+		topFuncBtns,
+		layout.NewSpacer(),    // 添加弹性空间
+		widget.NewSeparator(), // 动态分割线
+		btmFuncBtns,
+	)
+}
+
+// CreateDatasets 创建数据集管理界面
 func (ui *MainUI) showContent(content *fyne.Container) {
 	if ui.currentContent != nil {
 		ui.currentContent.Hide()
